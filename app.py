04af15746af4f7248634f0b96098ccabd93af282
gin <takaoueda1998@gmail.com>
2019-11-14 12:39:36 +0900

    add func:callback

--- app.py
+++ app.py
@@ -23,6 +23,23 @@ handler = WebhookHandler(CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #https://developers.line.biz/console/channel/1653365219/basic/
@@ -36,5 +53,4 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
